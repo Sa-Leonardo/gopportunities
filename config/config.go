fec71fd4d207aac653f2eed21e322e50cc1db24b
Sa-Leonardo <saleonardo3636gmail.com>
2025-08-05 17:42:55 -0300

    feat: add custom logger

---
+++ config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	return nil
+}
+
+
+func GETLogger(p string) *Logger {
+	// Initialize Logger
+
+	logger = NewLogger(p)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/Sa-Leonardo/gopportunities/router"
+import (
+	"github.com/Sa-Leonardo/gopportunities/config"
+	"github.com/Sa-Leonardo/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 // sempre que u não der um nome para o import, ele vai ser importado pelo ultimo pathName do modulo (gin	// sempre que u não der um nome para o import, ele vai ser importado pelo ultimo pathName do modulo, nesse caso (gin
 
 func main() {
-	// Initialize raouter
 
-  router.Initialize()
+	logger = config.GETLogger("main")
+
+	// Initialize configs
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config Inicialization error: %s", err)
+		return
+	}
+
+	// Initialize raouter
+	router.Initialize()
 }
