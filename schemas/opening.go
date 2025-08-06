7b45d17d2d95f9a529fe7377bc04f6b90d10aea7
Sa-Leonardo <saleonardo3636gmail.com>
2025-08-06 17:43:28 -0300

    feat: add struct OpeningResponse

--- schemas/opening.go
+++ schemas/opening.go
@@ -2,16 +2,30 @@ package schemas
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 // Project component structure
 
 type Opening struct {
 	gorm.Model
-	Role string
-	Company string
+	Role     string
+	Company  string
 	Location string
-	Remote bool
-	Link string
-	Salary int64
+	Remote   bool
+	Link     string
+	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdateAt  time.Time `json:"updateAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }
