9c7d2ad47e8e7c60a8fbdef5652e4550371af6ee
Sa-Leonardo <saleonardo3636gmail.com>
2025-09-23 09:19:40 -0300

    feat: add createdOpening handler logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -2,27 +2,43 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/Sa-Leonardo/gopportunities/schemas"
 	"net/http"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	request := struct {
-		Role string `json:"role"`
-	}{}
 
-	// Faz o bind do JSON
+	request := CreateOpeningRequest{}
+
+	// Faz o bind do JSON para popular o meu request
+	// ctx.BindJSON(&request)
+
+	//Faz o bind do JSON para popular o meu request e já faz a verificação erro de formato do body
 	if err := ctx.BindJSON(&request); err != nil {
-		logger.Errorf("erro no bind JSON: %v", err)
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": "malformed request body"})
+        return
+    }
+
+	if err := request.Validate(); err != nil {
+		logger.Error("validate error:", err.Error()) // loga no terminal
+		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	// Loga o request recebido
-	logger.Infof("request received: %+v", request)
+	opening := schemas.Opening{
+		Role: request.Role,
+		Company: request.Company,
+		Location: request.Location,
+		Remote: *request.Remote,
+		Link: request.Link,
+		Salary: request.Salary,
+	}
 
-	// Retorna uma resposta JSON
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "vaga criada com sucesso",
-		"data":    request,
-	})
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error creating opening %v", err.Error())
+		sendError(ctx,http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+	
+	sendSucess(ctx, "create-opening", opening)
 }
