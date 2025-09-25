b6e6907d77e6494ff03478d94b648d1852425235
Sa-Leonardo <saleonardo3636gmail.com>
2025-09-25 10:07:33 -0300

    feat: add update and request opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -42,3 +42,23 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+//update opening
+
+type UpdateOpeningRequest struct {
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    *bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	//if any fild is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	//if none of the fields were provided, return falsy
+	return fmt.Errorf("at least one field must be provided")
+}
